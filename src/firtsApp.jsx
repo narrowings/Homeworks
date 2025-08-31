bc3cf93d4d94bca8fd126a7d4eb9a242e70df30a
narrowings <rafael.plazas@uao.edu.co>
2025-08-31 00:54:11 -0500

    challenge#2

--- src/firtsApp.jsx
+++ src/firtsApp.jsx
@@ -1,28 +1,9 @@
-import { useState } from "react"
-
-const FirstApp = ({value}) => {
-
-    const [counter, setCounter] = useState(value)  
+const FirstApp = () => {
     
-    const handleAdd = () => {
-        setCounter(counter + 1)
-    }
-
-    const handleSubstract = () => {
-        setCounter(counter - 1)
-    }
-
-    const handleReset = () => {
-        setCounter (value)
-    }
-
     return(
     <>
-        <h1> Counter </h1>
-        <span> {counter} </span>
-        <button onClick={ () => handleAdd() }> +1 </button>
-        <button onClick={ () => handleSubstract() }> -1 </button>
-        <button onClick={ () => handleReset() }> Reset </button>
+        <h1> Hello world! :DD </h1>
+        <span> 10 </span>
     </>
     )
     }
