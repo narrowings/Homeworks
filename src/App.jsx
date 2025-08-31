4ab424863718bc9854f53c7d696e66aceb4ec247
narrowings <rafael.plazas@uao.edu.co>
2025-08-31 00:43:23 -0500

    challenge #3

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+const App = () => {
+  return(
+    <>
+      <h1> HOLA Mundo!! </h1>
+      <h1> Bienvenido </h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import FirstApp from './firtsApp.jsx'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <FirstApp value={0}/>
+  </StrictMode>,
+)
